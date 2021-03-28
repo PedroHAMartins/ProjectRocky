d714469ad921194b467c0510c2ec45416a9f1e0e
PedroHAMartins <48797255+PedroHAMartins@users.noreply.github.com>
2021-03-28 04:41:28 -0300

    First upload
    First commit to the repository, containing all the basic files from the initial development

---
+++ script/script_index_homepage.js
@@ -0,0 +1,231 @@
+var image1 = document.getElementById("product1_image");
+var image2 = document.getElementById("product2_image");
+var image3 = document.getElementById("product3_image");
+var image4 = document.getElementById("product4_image");
+var image5 = document.getElementById("product5_image");
+var image6 = document.getElementById("product6_image");
+var image7 = document.getElementById("product7_image");
+var image8 = document.getElementById("product8_image");
+var image9 = document.getElementById("product9_image");
+var image10 = document.getElementById("product10_image");
+var image11 = document.getElementById("product11_image");
+var image12 = document.getElementById("product12_image");
+
+
+
+function changeImage1(id){
+    if(document.getElementById(id).getAttribute("src") == "images/male_shirt1.png"){
+        document.getElementById(id).setAttribute("src", "images/male_shirt1_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/male_shirt1.png");
+    }
+}
+
+function changeImageLeave1(id){
+    if(document.getElementById(id).getAttribute("src") == "images/male_shirt1.png"){
+        document.getElementById(id).setAttribute("src", "images/male_shirt1_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/male_shirt1.png");
+    }
+}
+
+function changeImage2(id){
+    if(document.getElementById(id).getAttribute("src") == "images/female_shirt2.png"){
+        document.getElementById(id).setAttribute("src", "images/female_shirt2_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/female_shirt2.png");
+    }
+}
+
+function changeImageLeave2(id){
+    if(document.getElementById(id).getAttribute("src") == "images/female_shirt2.png"){
+        document.getElementById(id).setAttribute("src", "images/female_shirt2_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/female_shirt2.png");
+    }
+}
+
+function changeImage3(id){
+    if(document.getElementById(id).getAttribute("src") == "images/female_shirt1.png"){
+        document.getElementById(id).setAttribute("src", "images/female_shirt1_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/female_shirt1.png");
+    }
+}
+
+function changeImageLeave3(id){
+    if(document.getElementById(id).getAttribute("src") == "images/female_shirt1.png"){
+        document.getElementById(id).setAttribute("src", "images/female_shirt1_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/female_shirt1.png");
+    }
+}
+
+function changeImage4(id){
+    if(document.getElementById(id).getAttribute("src") == "images/male_shirt3.png"){
+        document.getElementById(id).setAttribute("src", "images/male_shirt3_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/male_shirt3.png");
+    }
+}
+
+function changeImageLeave4(id){
+    if(document.getElementById(id).getAttribute("src") == "images/male_shirt3.png"){
+        document.getElementById(id).setAttribute("src", "images/male_shirt3_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/male_shirt3.png");
+    }
+}
+
+function changeImage5(id){
+    if(document.getElementById(id).getAttribute("src") == "images/female_shirt3.png"){
+        document.getElementById(id).setAttribute("src", "images/female_shirt3_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/female_shirt3.png");
+    }
+}
+
+function changeImageLeave5(id){
+    if(document.getElementById(id).getAttribute("src") == "images/female_shirt3.png"){
+        document.getElementById(id).setAttribute("src", "images/female_shirt3_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/female_shirt3.png");
+    }
+}
+
+function changeImage6(id){
+    if(document.getElementById(id).getAttribute("src") == "images/male_shirt2.png"){
+        document.getElementById(id).setAttribute("src", "images/male_shirt2_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/male_shirt2.png");
+    }
+}
+
+function changeImageLeave6(id){
+    if(document.getElementById(id).getAttribute("src") == "images/male_shirt2.png"){
+        document.getElementById(id).setAttribute("src", "images/male_shirt2_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/male_shirt2.png");
+    }
+}
+
+function changeImage7(id){
+    if(document.getElementById(id).getAttribute("src") == "images/anilha1.png"){
+        document.getElementById(id).setAttribute("src", "images/anilha1_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/anilha1.png");
+    }
+}
+
+function changeImageLeave7(id){
+    if(document.getElementById(id).getAttribute("src") == "images/anilha1.png"){
+        document.getElementById(id).setAttribute("src", "images/anilha1_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/anilha1.png");
+    }
+}
+
+function changeImage8(id){
+    if(document.getElementById(id).getAttribute("src") == "images/esteira1.png"){
+        document.getElementById(id).setAttribute("src", "images/esteira1_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/esteira1.png");
+    }
+}
+
+function changeImageLeave8(id){
+    if(document.getElementById(id).getAttribute("src") == "images/esteira1.png"){
+        document.getElementById(id).setAttribute("src", "images/esteira1_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/esteira1.png");
+    }
+}
+
+function changeImage9(id){
+    if(document.getElementById(id).getAttribute("src") == "images/sapatilha1.png"){
+        document.getElementById(id).setAttribute("src", "images/sapatilha1_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/sapatilha1.png");
+    }
+}
+
+function changeImageLeave9(id){
+    if(document.getElementById(id).getAttribute("src") == "images/sapatilha1.png"){
+        document.getElementById(id).setAttribute("src", "images/sapatilha1_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/sapatilha1.png");
+    }
+}
+
+function changeImage10(id){
+    if(document.getElementById(id).getAttribute("src") == "images/bermuda_inf1.png"){
+        document.getElementById(id).setAttribute("src", "images/bermuda_inf1_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/bermuda_inf1.png");
+    }
+}
+
+function changeImageLeave10(id){
+    if(document.getElementById(id).getAttribute("src") == "images/bermuda_inf1.png"){
+        document.getElementById(id).setAttribute("src", "images/bermuda_inf1_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/bermuda_inf1.png");
+    }
+}
+
+function changeImage11(id){
+    if(document.getElementById(id).getAttribute("src") == "images/chuteira_inf1.png"){
+        document.getElementById(id).setAttribute("src", "images/chuteira_inf1_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/chuteira_inf1.png");
+    }
+}
+
+function changeImageLeave11(id){
+    if(document.getElementById(id).getAttribute("src") == "images/mchuteira_inf1.png"){
+        document.getElementById(id).setAttribute("src", "images/chuteira_inf1_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/chuteira_inf1.png");
+    }
+}
+
+function changeImage12(id){
+    if(document.getElementById(id).getAttribute("src") == "images/bola_basquete1.png"){
+        document.getElementById(id).setAttribute("src", "images/bola_basquete1_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/bola_basquete1.png");
+    }
+}
+
+function changeImageLeave12(id){
+    if(document.getElementById(id).getAttribute("src") == "images/bola_basquete1.png"){
+        document.getElementById(id).setAttribute("src", "images/bola_basquete1_info.png");
+    }
+    else{
+        document.getElementById(id).setAttribute("src", "images/bola_basquete1.png");
+    }
+}
+
